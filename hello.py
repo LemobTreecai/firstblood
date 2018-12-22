0c1f5ed5660b0c63f6fec0f37ddc88ee4c716814
CAI <869655681@qq.com>
2018-12-22 05:47:09 -0500

    99mixus

--- hello.py
+++ hello.py
@@ -3,3 +3,8 @@ sum  = 0
 for i in range(1, 100):
     sum += i 
 print("e====no sum")
+
+for row in range(1, 10):
+    for col in range(1, row+1):
+        print(row,"*",col,"=",(row*col),end="\t")
+    print()
